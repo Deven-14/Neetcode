1ca0960480721a0a9a79ad2d270a2a84a16335ab
Deven-14 <deven.paramaj@gmail.com>
2025-03-05 22:58:33 +0530

    sol: Products of Array Except Self

--- Arrays and Hashing/Products of Array Except Self/solution.py
+++ Arrays and Hashing/Products of Array Except Self/solution.py
@@ -18,3 +18,21 @@ class Solution:
         products.append(pre[-2])
 
         return products
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        products = [1] * n
+
+        prefix = 1
+        for i in range(n):
+            products[i] = prefix
+            prefix *= nums[i]
+        
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            products[i] *= suffix
+            suffix *= nums[i]
+        
+        return products
