b5bfd18fa45221873fdba35f58b91fc2a255528b
Deven-14 <deven.paramaj@gmail.com>
2025-03-08 00:41:26 +0530

    sol: Longest Repeating Character Replacement

---
+++ Sliding Window/Longest Repeating Character Replacement/solution.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        counts = defaultdict(int)
+        max_length = 0
+        left = 0
+        max_freq = 0
+
+        for right in range(len(s)):
+            counts[s[right]] += 1
+            max_freq = max(max_freq, counts[s[right]])
+
+            while (right - left + 1) - max_freq > k:
+                counts[s[left]] -= 1
+                left += 1
+            
+            max_length = max(max_length, right - left + 1)
+        
+        return max_length
