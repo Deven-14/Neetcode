bfd32fb3382cd8773d8fb22e4f16e6235fd94d42
Deven-14 <deven.paramaj@gmail.com>
2025-03-13 23:04:25 +0530

    sol: Subsets

---
+++ Backtracking/Subsets/solution.py
@@ -0,0 +1,49 @@
+class Solution:
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        sub_sets = [[]]
+        
+        for num in nums:
+            new_subsets = []
+            
+            for subset in sub_sets:
+                new_subset = list(subset)
+                new_subset.append(num)
+                new_subsets.append(new_subset)
+
+            sub_sets.extend(new_subsets)
+        
+        return sub_sets
+
+
+class Solution:
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        sub_sets = [[]]
+        
+        for num in nums:
+            sub_sets += [ subset + [num] for subset in sub_sets]
+        
+        return sub_sets
+
+
+class Solution:
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        sub_sets = []
+        subset = []
+        n = len(nums)
+
+        def dfs(i):
+            if i == n:
+                sub_sets.append(list(subset))
+                return
+            
+            subset.append(nums[i])
+            dfs(i + 1)
+            subset.pop()
+            dfs(i + 1)
+        
+        dfs(0)
+        return sub_sets
+
