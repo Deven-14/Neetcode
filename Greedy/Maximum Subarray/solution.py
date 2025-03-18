5a7070d60412496d96223bb48da615812b346761
Deven-14 <deven.paramaj@gmail.com>
2025-03-18 20:49:14 +0530

    sol: Maximum Subarray

---
+++ Greedy/Maximum Subarray/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # kandane'a algo
+        curr_max = nums[0]
+        total_max = nums[0]
+        for i in range(1, len(nums)):
+            curr_max = max(curr_max + nums[i], nums[i])
+            total_max = max(curr_max, total_max)
+        
+        return total_max
+
+
+# * same answer using DP
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp = [*nums]
+        for i in range(1, len(nums)):
+            dp[i] = max(nums[i], dp[i - 1] + nums[i])
+        
+        return max(dp)
