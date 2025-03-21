49d4789a265d5c273c5fcdcfca499800df8677ea
Deven-14 <deven.paramaj@gmail.com>
2025-03-21 19:03:02 +0530

    sol: Course Schedule II - detect cycle with separate visited and cycle variables

--- Graphs/Course Schedule II/solution.py
+++ Graphs/Course Schedule II/solution.py
@@ -69,10 +69,44 @@ class Solution:
                 return [] # indicating that the solution is not possible
         
         return course_order
-    
 
 # * cycle detection method using DFS
 # * Time Complexity: O(V + E)
+
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = [[] for _ in range(numCourses)] # Graph represented as an adjacency list
+
+        for course, dependent_course in prerequisites:
+            adj[course].append(dependent_course)
+        
+        def detect_cycle(course, cycle, course_order): # dfs
+            if course in cycle:
+                return cycle[course] # returns True if cycle is present, else False, also behaves as visited = {}, if course is present in cycle, then it is visited (could be True or False)
+            
+            cycle[course] = True
+            for dependent_course in adj[course]:
+                if detect_cycle(dependent_course, cycle, course_order):
+                    return True
+            
+            cycle[course] = False
+            course_order.append(course)
+            
+            return False
+        
+        cycle = {} # visited
+        course_order = []
+        for course in range(len(adj)):
+            if detect_cycle(course, cycle, course_order):
+                return [] # indicating that the solution is not possible
+        
+        return course_order
+    
+
+# * cycle detection method using DFS without separate visited and cycle 
+# * Time Complexity: O(V + E)
+
 
 
 class Solution:
