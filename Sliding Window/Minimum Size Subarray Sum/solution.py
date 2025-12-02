5c58a216d5131e215df4dd44e4e3abef205a4083
Deven-14 <deven.paramaj@gmail.com>
2025-12-02 21:30:27 +0530

    sol: Minimum Size Subarray Sum

---
+++ Sliding Window/Minimum Size Subarray Sum/solution.py
@@ -0,0 +1,19 @@
+from collections import deque
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        if sum(nums) < target:
+            return 0
+        
+        min_length = len(nums)
+        window = deque()
+        window_sum = 0
+
+        for num in nums:
+            window.append(num)
+            window_sum += num
+
+            while window_sum >= target:
+                min_length = min(min_length, len(window))
+                window_sum -= window.popleft()
+
+        return min_length
