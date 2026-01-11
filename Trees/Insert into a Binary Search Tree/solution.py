446b96add07ad7424100613d1df6f51a9c4b673f
Deven-14 <deven.paramaj@gmail.com>
2026-01-11 21:56:07 +0530

    sol: Insert into a Binary Search Tree

---
+++ Trees/Insert into a Binary Search Tree/solution.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        elif val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        
+        return root
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        
+        node = root
+        prev = None
+
+        while node:
+            prev = node
+            node = node.left if val < node.val else node.right
+        
+        node = prev
+        new_node = TreeNode(val)
+        if val < node.val:
+            node.left = new_node
+        else:
+            node.right = new_node
+        
+        return root
