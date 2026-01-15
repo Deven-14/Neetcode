b8c5967d6c75ba0f6a7b3325c0d527bc4d666c48
Deven-14 <deven.paramaj@gmail.com>
2026-01-15 11:23:31 +0530

    sol: Single Threaded CPU

--- Heap and Priority Queue/Single Threaded CPU/solution.py
+++ Heap and Priority Queue/Single Threaded CPU/solution.py
@@ -24,3 +24,37 @@ class Solution:
                 current_time = min_heap_tasks[0][0]
 
         return order
+
+
+
+
+import heapq
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        tasks = [task + [i] for i, task in enumerate(tasks)]
+        tasks.sort(key=lambda x: x[0]) # sort on enqueue time
+        i, n = 0, len(tasks)
+        
+        min_heap = []
+        order = []
+        current_time = tasks[0][0]
+
+        while i < n or min_heap:
+
+            while i < n and tasks[i][0] <= current_time:
+                (enqueue_time, processing_time, idx) = tasks[i]
+                heapq.heappush(min_heap, (processing_time, idx))
+                i += 1
+            
+            if min_heap:
+                processing_time, idx = heapq.heappop(min_heap)
+                current_time += processing_time
+                order.append(idx)
+        
+            else:
+                current_time = tasks[i][0]
+
+        return order
+    
+
+
