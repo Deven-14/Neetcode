fcbac9cb351cac2d481fa579f79818b8d558ae74
Deven-14 <deven.paramaj@gmail.com>
2026-01-18 09:18:30 +0530

    sol: Car Pooling

--- Heap and Priority Queue/Car Pooling/solution.py
+++ Heap and Priority Queue/Car Pooling/solution.py
@@ -19,3 +19,23 @@ class Solution:
         return True
 
 
+import heapq
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        L = min(start for _, start, _ in trips)
+        R = max(end for _, _, end in trips)
+
+        N = R - L + 1
+        passenger_changes = [0] * (N + 1)
+        for passengers, start, end in trips:
+            passenger_changes[start - L] += passengers
+            passenger_changes[end - L] -= passengers
+        
+        current_passengers = 0
+        for change in passenger_changes:
+            current_passengers += change
+            if current_passengers > capacity:
+                return False
+        
+        return True
+
