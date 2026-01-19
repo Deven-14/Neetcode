87d2caf7e57e7fcfc313318110e5682f6e66d91a
Deven-14 <deven.paramaj@gmail.com>
2026-01-19 08:46:40 +0530

    sol: Combinations

--- Backtracking/Combinations/solution.py
+++ Backtracking/Combinations/solution.py
@@ -17,3 +17,25 @@ class Solution:
         helper(0, 0)
         return combinations
 
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        combinations = []
+        comb = []
+        nums = list(range(1, n+1))
+
+        def helper(i):
+            if len(comb) == k:
+                combinations.append(list(comb))
+                return
+            
+            for j in range(i, n):
+                comb.append(nums[j])
+                helper(j + 1)
+                comb.pop()
+        
+        helper(0)
+        return combinations
+
+
