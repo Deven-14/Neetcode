63c7a9b3acf7db18c2289c36bab93dfd475fa124
Deven-14 <deven.paramaj@gmail.com>
2026-01-22 10:56:03 +0530

    sol: Word Search II

--- Tries/Word Search II/solution.py
+++ Tries/Word Search II/solution.py
@@ -126,3 +126,79 @@ class Solution:
                 check_backtrack(i, j, prefix_tree.head)
         
         return found_words
+    
+
+
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.parent = None
+        self.word_idx = -1
+        self.refs = 0
+
+class PrefixTree:
+    def __init__(self, words):
+        self.head = Node()
+
+        self.words = words
+        for i, word in enumerate(words):
+            self._add(word, i)
+            
+    def _add(self, word, i):
+        node = self.head
+        for char in word:
+
+            if char not in node.children:
+                new_node = Node()
+                new_node.parent = node
+                node.children[char] = new_node
+
+            node = node.children[char]
+            node.refs += 1
+
+        node.word_idx = i
+    
+    def reduce_refs(self, node):
+        while node:
+            node.refs -= 1
+            node = node.parent
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        prefix_tree = PrefixTree(words)
+
+        n = len(board)
+        m = len(board[0])
+        found_words = []
+
+        def check_backtrack(i, j, node, visited=set()):
+            if i < 0 or i >= n or j < 0 or j >= m:
+                return
+            
+            if (i, j) in visited:
+                return 
+
+            char = board[i][j]
+            if char not in node.children or node.children[char].refs == 0:
+                return 
+            
+            visited.add((i, j))
+
+            node = node.children[char]
+            if node.word_idx != -1:
+                word = words[node.word_idx]
+                found_words.append(word)
+                node.word_idx = -1
+                prefix_tree.reduce_refs(node)
+
+            paths = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]
+            for (x, y) in paths:
+                check_backtrack(x, y, node, visited)
+            
+            visited.remove((i, j))
+        
+        for i in range(n):
+            for j in range(m):
+                check_backtrack(i, j, prefix_tree.head)
+        
+        return found_words
