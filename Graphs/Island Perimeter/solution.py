d378b7abfd71cd7fe02f34801b403474865ebffe
Deven-14 <deven.paramaj@gmail.com>
2026-01-24 09:46:19 +0530

    sol: Island Perimeter

--- Graphs/Island Perimeter/solution.py
+++ Graphs/Island Perimeter/solution.py
@@ -29,4 +29,28 @@ class Solution:
                 
                 
 
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+
+        def dfs(i, j, visited = set()):
+            if (i < 0 or i >= ROWS or j < 0 or j >= COLS or grid[i][j] == 0):
+                return 1
+            
+            if (i, j) in visited:
+                return 0
+
+            visited.add((i, j))
+            perimeter = dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1)
+            
+            return perimeter
+            
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 1:
+                    return dfs(i, j)
         
+        return 0
+                
+                
