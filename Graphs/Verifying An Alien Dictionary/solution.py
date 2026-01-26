96ed2e5489deba077780af54b2b27c3535651ef2
Deven-14 <deven.paramaj@gmail.com>
2026-01-26 13:04:44 +0530

    sol: Verifying An Alien Dictionary

--- Graphs/Verifying An Alien Dictionary/solution.py
+++ Graphs/Verifying An Alien Dictionary/solution.py
@@ -29,4 +29,33 @@ class Solution:
         
         return dfs(1, 0)
 
+
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_map = { char: idx for idx, char in enumerate(order) }
         
+        def dfs(i, j):
+            if i == len(words):
+                return True
+            
+            n, m = len(words[i]), len(words[i-1])
+            if m == j <= n:
+                return True
+            
+            elif j == n and j < m:
+                return False
+
+            om, on = order_map[words[i-1][j]], order_map[words[i][j]]
+            if om > on:
+                return False
+            
+            elif om == on:
+                return dfs(i, j+1) and dfs(i+1, 0)
+            
+            return dfs(i + 1, 0)
+
+        if len(words) == 1:
+            return True
+        
+        return dfs(1, 0)
