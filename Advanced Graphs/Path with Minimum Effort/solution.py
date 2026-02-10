1e0be903f3827e0f15be5dc6034a1df85f668750
Deven-14 <deven.paramaj@gmail.com>
2026-02-10 23:17:45 +0530

    sol: Path with Minimum Effort

--- Advanced Graphs/Path with Minimum Effort/solution.py
+++ Advanced Graphs/Path with Minimum Effort/solution.py
@@ -79,4 +79,77 @@ class Solution:
                 heapq.heappush(min_heap, (new_min_effort, x, y))
 
         return 0
+
+
+# ! maximum recursion depth
+
+import heapq
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        ROWS, COLS = len(heights), len(heights[0])
         
+        def dfs(i, j, limit, visited):
+            if i == ROWS-1 and j == COLS-1:
+                return True
+            
+            visited[i][j] = True
+            routes = ((i+1, j), (i-1, j), (i, j+1), (i, j-1))
+
+            for x, y in routes:
+                if x < 0 or x >= ROWS or y < 0 or y >= COLS or visited[x][y] or abs(heights[i][j] - heights[x][y]) > limit:
+                    continue
+                if dfs(x, y, limit, visited):
+                    return True
+            
+            return False
+
+        l, r = 0, max(max(row) for row in heights)
+        min_effort = float("inf")
+        
+        while l <= r:
+            mid = l + (r - l) // 2
+            visited = [[False] * COLS for _ in range(ROWS)]
+            if dfs(0, 0, mid, visited):
+                min_effort = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        
+        return min_effort
+
+
+# * slower than Dijkstra's Algorithm
+import heapq
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        ROWS, COLS = len(heights), len(heights[0])
+        
+        def dfs_can_reach(limit):
+            visited = [[False] * COLS for _ in range(ROWS)]
+            stack = [(0, 0)]
+
+            while stack:
+                i, j = stack.pop()
+                if i == ROWS-1 and j == COLS-1:
+                    return True
+            
+                visited[i][j] = True
+                routes = ((i+1, j), (i-1, j), (i, j+1), (i, j-1))
+                for x, y in routes:
+                    if x < 0 or x >= ROWS or y < 0 or y >= COLS or visited[x][y] or abs(heights[i][j] - heights[x][y]) > limit:
+                        continue
+                    stack.append((x, y))
+        
+        
+        l, r = 0, max(max(row) for row in heights)
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            if dfs_can_reach(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+        
+        return l
+        
+
