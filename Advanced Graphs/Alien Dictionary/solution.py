f345497f7e2c1d8849f142d8cf7d30b4061e561b
Deven-14 <deven.paramaj@gmail.com>
2026-02-13 11:15:01 +0530

    sol: Alien Dictionary

--- Advanced Graphs/Alien Dictionary/solution.py
+++ Advanced Graphs/Alien Dictionary/solution.py
@@ -54,3 +54,63 @@ class Solution:
 # * we could do this using topological sort BFS too, 
 # * in that case if there is cycle, then the order will not be equal to the number of nodes
 # * so we can check if len(order) == len(adj), if not, then return ""
+
+
+from collections import deque
+class Solution:
+
+    def create_adj_list(self, words):
+        adj = { c: set() for w in words for c in w }
+        indegrees = { c: 0 for c in adj }
+        n = len(words)
+        
+        # create adj list
+        prev_word = words[0]
+        for i in range(1, n):
+            word = words[i]
+
+            n = min(len(prev_word), len(word))
+            if len(prev_word) > len(word) and prev_word[:n] == word[:n]: # ["apple world", "apple"]
+                return None, None
+            
+            for i in range(n):
+                if prev_word[i] != word[i]:
+                    if word[i] not in adj[prev_word[i]]:
+                        indegrees[word[i]] += 1
+                        adj[prev_word[i]].add(word[i])
+                    break # first mis-matching char, it might not be present as well, ["apple", "apples"]
+            
+            prev_word = word
+
+        return adj, indegrees
+
+    def bfs(self, adj, indegrees):
+        queue = deque([c for c, indegree in indegrees.items() if indegree == 0])
+        order = []
+
+        while queue:
+            c = queue.popleft()
+            order.append(c)
+
+            for adj_c in adj[c]:
+                indegrees[adj_c] -= 1
+                if indegrees[adj_c] == 0:
+                    queue.append(adj_c)
+        
+        if len(order) != len(adj):
+            return None
+        
+        return order[::-1]
+
+
+    def foreignDictionary(self, words: List[str]) -> str:
+        # Topological sorting with cycle detection
+        adj, indegrees = self.create_adj_list(words)
+        if not adj:
+            return ""
+        
+        order = self.bfs(adj, indegrees)
+        if not order:
+            return ""
+
+        return "".join(order[::-1])
