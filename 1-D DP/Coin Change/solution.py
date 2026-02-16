7ef77b862a6461241a2fbb9da3e24a19e2b6f1c0
Deven-14 <deven.paramaj@gmail.com>
2026-02-16 20:48:28 +0530

    sol: Coin Change

--- 1-D DP/Coin Change/solution.py
+++ 1-D DP/Coin Change/solution.py
@@ -9,3 +9,15 @@ class Solution:
                     dp[curr_amt] = min(dp[curr_amt], 1 + dp[curr_amt - coin])
         
         return dp[amount] if dp[amount] != amount+1 else -1
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount+1] * (amount + 1)
+        dp[0] = 0
+        
+        for coin in coins:
+            for curr_amt in range(coin, amount+1):
+                dp[curr_amt] = min(dp[curr_amt], 1 + dp[curr_amt - coin])
+        
+        return dp[amount] if dp[amount] != amount+1 else -1
