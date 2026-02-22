efedb9eef3c64573a0cd71792325471e163c3dd4
Deven-14 <deven.paramaj@gmail.com>
2026-02-22 14:31:34 +0530

    sol: Stone Game II

--- 2-D DP/Stone Game II/solution.py
+++ 2-D DP/Stone Game II/solution.py
@@ -34,3 +34,31 @@ class Solution:
             return min_stones
 
         return dp(0, 1, True)
+
+
+from functools import cache
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        n = len(piles)
+        suffix_sum = [0] * n
+        suffix_sum[-1] = piles[-1]
+        for i in range(n - 2, -1, -1):
+            suffix_sum[i] = piles[i] + suffix_sum[i + 1]
+        
+        @cache
+        def dp(i, m):
+            if i >= n:
+                return 0
+        
+            max_stones = 0
+            count = 1
+            for j in range(i, min(n, i + 2 * m)):
+                max_stones = max(
+                    max_stones, 
+                    suffix_sum[i] - dp(j + 1, max(count, m))
+                )
+                count += 1
+
+            return max_stones
+
+        return dp(0, 1)
