4f9e83d33985ec313301789c0acdbf53af846171
Deven-14 <deven.paramaj@gmail.com>
2026-02-22 13:54:12 +0530

    sol: Stone Game

---
+++ 2-D DP/Stone Game/solution.py
@@ -0,0 +1,15 @@
+from functools import cache
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        
+        @cache
+        def dp(i, j):
+            if i == j:
+                return 0
+            
+            return max(
+                piles[i] - dp(i + 1, j),
+                piles[j] - dp(i, j - 1)
+            )
+        
+        return dp(0, len(piles) - 1) > 0
