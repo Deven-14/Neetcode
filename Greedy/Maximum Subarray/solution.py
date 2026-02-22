a912f88d87ec0ad85a2b015a163594bdb155cbc7
Deven-14 <deven.paramaj@gmail.com>
2026-02-22 16:05:39 +0530

    sol: Maximum Subarray

--- Greedy/Maximum Subarray/solution.py
+++ Greedy/Maximum Subarray/solution.py
@@ -19,3 +19,14 @@ class Solution:
             dp[i] = max(nums[i], dp[i - 1] + nums[i])
         
         return max(dp)
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+        for i in range(1, n):
+            dp[i] = max(nums[i], dp[i - 1] + nums[i])
+        
+        return max(dp)
