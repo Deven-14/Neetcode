521357e551adc0f0c2c7bdc81792cc906e4db407
Deven-14 <deven.paramaj@gmail.com>
2026-02-25 10:26:23 +0530

    sol: Greatest Common Divisor of Strings

---
+++ Math & Geometry/Greatest Common Divisor of Strings/solution.py
@@ -0,0 +1,8 @@
+from math import gcd
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        l1, l2 = len(str1), len(str2)
+        divisor = gcd(l1, l2)
+        return str1[:divisor]
